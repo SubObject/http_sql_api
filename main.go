24a9c3971e33078cafeda95bd4d9f0e37f287614
qindong <herenshan112@163.com>
2018-12-11 18:15:53 +0800

    加载基础配置

--- main.go
+++ main.go
@@ -2,8 +2,24 @@ package main
 
 import (
 	"fmt"
+	"flag"
+	"os"
+
+	"http_sql_api/config"
+)
+
+var (
+	systemConfigFiles = flag.String("configFile","conf/appConfig.ini","App配置文件")
 )
 
 func main() {
-	fmt.Println("启动程序")
+	fmt.Println("启动程序,开始加载基础配置...")
+	flag.Parse()
+	
+	configSetUp, err := config.LoadSystemConfig(*systemConfigFiles)
+	fmt.Println(configSetUp)
+	if err != nil {
+		os.Exit(3)
+	}
+	
 }
